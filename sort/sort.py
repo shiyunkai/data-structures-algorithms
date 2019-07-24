c0164faea1265e500cec0241acfd75eb285a222c
shiyunkai <44898778+shiyunkai@users.noreply.github.com>
2019-07-24 18:24:30 +0800

    imooc 排序算法

---
+++ sort/sort.py
@@ -0,0 +1,24 @@
+"""
+	参考文章url:https://www.imooc.com/article/288482
+"""
+
+def select_sort(arr):
+	"""选择排序，从开头元素的后面选择一个比开头元素小的数，找到后和开头元素交换"""
+	arr_len = len(arr)
+	for i in range(arr_len-1):
+		min_index = i
+		for j in range(i+1,arr_len):
+			if(arr[min_index]>arr[j]):
+				min_index = j
+		arr[i], arr[min_index] = arr[min_index], arr[i]		
+	return arr
+	
+def insert_sort(arr):
+	"""插入排序，
+		1. 将序列分成已排序和未排序两部分，
+		2. 每次从未排序序列中选择一个，找到合适的位置，插入到已排序序列中
+	"""
+
+if __name__ == "__main__":
+	arr = [1,0,5,3,7,4,-3,100]
+	print(select_sort(arr))
